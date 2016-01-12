cfcca5ca25dd824049f0ff89d71c836d5f03a5c8
Alasdair J. Barr <ajbarr@hotmail.com>
2016-01-12 14:10:21 -0500

    got pieces moving

--- lib/chess.rb
+++ lib/chess.rb
@@ -2,6 +2,7 @@ require_relative 'pieces.rb'
 
 class Chess
 	attr_accessor :board, :game_over, :turn
+	include Pieces
 
 	def initialize
 		@board = Hash.new
@@ -12,7 +13,7 @@ class Chess
 	def create_board
 		1.upto(8) do |x|
 			1.upto(8) do |y|
-				board["#{x}, #{y}"] = Space.new(x, y)
+				board["#{x}, #{y}"] = Pieces::Space.new
 			end
 		end
 	end
@@ -20,30 +21,30 @@ class Chess
 	def create_pieces
 		#create pawns
 		1.upto(8) do |x|
-			board["#{x}, 2"] = Pawn.new("white", x, 2, "wP")
-			board["#{x}, 7"] = Pawn.new("black", x, 7, "bP")
+			board["#{x}, 2"] = Pieces::Pawn.new("white", x, 2, "wP")
+			board["#{x}, 7"] = Pieces::Pawn.new("black", x, 7, "bP")
 		end
 		#create rooks
-		board["1, 1"] = Rook.new("white", 1, 1, "wR")
-		board["8, 1"] = Rook.new("white", 8, 1, "wR")
-		board["1, 8"] = Rook.new("black", 1, 8, "bR")
-		board["8, 8"] = Rook.new("black", 8, 8, "bR")
+		board["1, 1"] = Pieces::Rook.new("white", 1, 1, "wR")
+		board["8, 1"] = Pieces::Rook.new("white", 8, 1, "wR")
+		board["1, 8"] = Pieces::Rook.new("black", 1, 8, "bR")
+		board["8, 8"] = Pieces::Rook.new("black", 8, 8, "bR")
 		#create knights
-		board["2, 1"] = Knight.new("white", 1, 1, "wK")
-		board["7, 1"] = Knight.new("white", 8, 1, "wK")
-		board["2, 8"] = Knight.new("black", 1, 8, "bK")
-		board["7, 8"] = Knight.new("black", 8, 8, "bK")
+		board["2, 1"] = Pieces::Knight.new("white", 1, 1, "wK")
+		board["7, 1"] = Pieces::Knight.new("white", 8, 1, "wK")
+		board["2, 8"] = Pieces::Knight.new("black", 1, 8, "bK")
+		board["7, 8"] = Pieces::Knight.new("black", 8, 8, "bK")
 		#create bishops
-		board["3, 1"] = Bishop.new("white", 1, 1, "wB")
-		board["6, 1"] = Bishop.new("white", 8, 1, "wB")
-		board["3, 8"] = Bishop.new("black", 1, 8, "bB")
-		board["6, 8"] = Bishop.new("black", 8, 8, "bB")
+		board["3, 1"] = Pieces::Bishop.new("white", 1, 1, "wB")
+		board["6, 1"] = Pieces::Bishop.new("white", 8, 1, "wB")
+		board["3, 8"] = Pieces::Bishop.new("black", 1, 8, "bB")
+		board["6, 8"] = Pieces::Bishop.new("black", 8, 8, "bB")
 		#create kings
-		board["5, 1"] = King.new("white", 1, 1, "WK")
-		board["5, 8"] = King.new("black", 1, 8, "BK")
+		board["5, 1"] = Pieces::King.new("white", 1, 1, "WK")
+		board["5, 8"] = Pieces::King.new("black", 1, 8, "BK")
 		#create queens
-		board["4, 1"] = Queen.new("white", 1, 1, "wQ")
-		board["4, 8"] = Queen.new("black", 1, 8, "bQ")
+		board["4, 1"] = Pieces::Queen.new("white", 1, 1, "wQ")
+		board["4, 8"] = Pieces::Queen.new("black", 1, 8, "bQ")
 	end
 
 	def show_board
@@ -74,9 +75,13 @@ class Chess
 	def move_valid(piece, move)
 		x = piece[0]
 		y = piece[1]
+		a = move[0]
+		b = move[1]
+		selected = board.fetch("#{x}, #{y}")
 
-		if board["#{x}, {y}"].possible_moves(piece).include? move
-
+		if selected.possible_moves(piece).include? move
+			board["#{a}, #{b}"] = board.fetch("#{x}, #{y}")
+			board["#{x}, #{y}"] = Pieces::Space.new
 		end
 
 	end
@@ -93,6 +98,7 @@ class Chess
 		end
 
 	end
+
 
 
 end
--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,105 +1,146 @@
-class Pieces
+module Pieces
 
-	attr_accessor :x, :y
-	attr_reader :symbol
+	class Piece
 
-	def initialize(colour, x, y, symbol)
-		@colour = colour
-		@x, @y = x, y
-	end
+		attr_accessor :x, :y
+		attr_reader :symbol, :colour
 
-end
-
-class Pawn < Pieces
-
-	attr_accessor :first_move
-
-	def initialize(colour, x, y, symbol)
-		super(colour, x, y, symbol)
-		@first_move = true
-		@symbol = symbol
-	end
-
-	def possible_moves(arr)
-		x = arr[0]
-		y = arr[1]
-			
-		if self.colour == "white"
-			if @first_move == true 
-				possibilities = [ [x, y+1], [x, y+2] ]
-			else
-				possibilities = [x, y+1]
-			end 
+		def initialize(colour, x, y, symbol)
+			@colour = colour
+			@x, @y = x, y
 		end
 
-		if self.colour == "black"
-			if @first_move == true 
-				possibilities = [ [x, y-1], [x, y-2] ]
-			else
-				possibilities = [x, y-1]
-			end 
+	end
+
+	class Pawn < Piece
+
+		attr_accessor :first_move
+
+		def initialize(colour, x, y, symbol)
+			super(colour, x, y, symbol)
+			@first_move = true
+			@symbol = symbol
+		end
+
+		def possible_moves(arr)
+			x = arr[0]
+			y = arr[1]
+				
+			if self.colour == "white"
+				if @first_move == true 
+					possibilities = [ [x, y+1], [x, y+2] ]
+				else
+					possibilities = [x, y+1]
+				end 
+			end
+
+			if self.colour == "black"
+				if @first_move == true 
+					possibilities = [ [x, y-1], [x, y-2] ]
+				else
+					possibilities = [x, y-1]
+				end 
+			end
+			
+			return possibilities
+		end
+
+	end
+
+	class Rook < Piece
+
+		def initialize(colour, x, y, symbol)
+			super(colour, x, y, symbol)
+			@first_move = true
+			@symbol = symbol
+		end
+
+		def possible_moves(arr)
+			x = arr[0]
+			y = arr[1]
+			options = [ [1, 0], [0, 1] ]
+			possiblities = []
+
+			options.each do |opt|
+				position = arr
+				until position.any? {|value| value <1} || position.any? {|value| value >8} || board["#{position[0]}, #{position[1]}"].instance_of? Piece 
+
+				end
+
+			end
+
+		end
+
+	end
+
+	class Knight < Piece
+
+		def initialize(colour, x, y, symbol)
+			super(colour, x, y, symbol)
+			@symbol = symbol
 		end
 		
+		def possible_moves(arr)
+			x = arr[0]
+			y = arr[1]
+
+		end
+
 	end
 
+	class Bishop < Piece
 
-end
+		def initialize(colour, x, y, symbol)
+			super(colour, x, y, symbol)
+			@symbol = symbol
+		end
+		
+		def possible_moves(arr)
+			x = arr[0]
+			y = arr[1]
 
-class Rook < Pieces
+		end
 
-	def initialize(colour, x, y, symbol)
-		super(colour, x, y, symbol)
-		@first_move = true
-		@symbol = symbol
 	end
 
-end
+	class Queen < Piece
 
-class Knight < Pieces
+		def initialize(colour, x, y, symbol)
+			super(colour, x, y, symbol)
+			@symbol = symbol
+		end
+		
+		def possible_moves(arr)
+			x = arr[0]
+			y = arr[1]
 
-	def initialize(colour, x, y, symbol)
-		super(colour, x, y, symbol)
-		@symbol = symbol
+		end
+
 	end
-	
-end
 
-class Bishop < Pieces
+	class King < Piece
 
-	def initialize(colour, x, y, symbol)
-		super(colour, x, y, symbol)
-		@symbol = symbol
+		def initialize(colour, x, y, symbol)
+			super(colour, x, y, symbol)
+			@first_move = true
+			@symbol = symbol
+		end
+		
+		def possible_moves(arr)
+			x = arr[0]
+			y = arr[1]
+
+		end
+
 	end
-	
-end
 
-class Queen < Pieces
+	class Space
+		attr_reader :symbol
 
-	def initialize(colour, x, y, symbol)
-		super(colour, x, y, symbol)
-		@symbol = symbol
-	end
-	
-end
+		def initialize
+			@symbol = "  "
+		end
 
-class King < Pieces
-
-	def initialize(colour, x, y, symbol)
-		super(colour, x, y, symbol)
-		@first_move = true
-		@symbol = symbol
-	end
-	
-end
-
-class Space
-	attr_accessor :x, :y
-	attr_reader :symbol
-
-	def initialize(x, y)
-		@x = x
-		@y = y
-		@symbol = "  "
 	end
 
 end
